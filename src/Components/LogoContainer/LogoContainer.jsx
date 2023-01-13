3fd074598f81d5fee4fd220493c260b1bc7750e1
KGS97 <45433869+KGS97@users.noreply.github.com>
2023-01-13 23:13:40 +0530

    changed logo image to relative path

--- src/Components/LogoContainer/LogoContainer.jsx
+++ src/Components/LogoContainer/LogoContainer.jsx
@@ -3,10 +3,7 @@ import style from "./LogoContainer.module.css";
 const LogoContainer = () => {
   return (
     <div className={style.LogoContainer}>
-      <img
-        alt="logo"
-        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-      />
+      <img alt="logo" src={process.env.PUBLIC_URL + "/images/logo.png"} />
     </div>
   );
 };
