6ab3c656f2be5380440588ce3e75cebd3e438443
KGS97 <45433869+KGS97@users.noreply.github.com>
2023-01-15 23:20:50 +0530

    everything alright at home

--- src/Components/LogoContainer/LogoContainer.jsx
+++ src/Components/LogoContainer/LogoContainer.jsx
@@ -3,7 +3,11 @@ import style from "./LogoContainer.module.css";
 const LogoContainer = () => {
   return (
     <div className={style.LogoContainer}>
-      <img alt="logo" src={process.env.PUBLIC_URL + "/images/logo.png"} />
+      <img
+        className={style.logo}
+        alt="logo"
+        src={process.env.PUBLIC_URL + "/images/logo.png"}
+      />
     </div>
   );
 };
--- src/Components/ResultContainer/ResultContainer.jsx
+++ src/Components/ResultContainer/ResultContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./ResultContainer.module.css";
 const ResultContainer = ({ link, displayLink, title, snippet }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <p className={styles.link}>{displayLink}</p>
       <a href={link} className={styles.anchor}>
         {title}
